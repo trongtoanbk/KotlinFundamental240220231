922bb18307c1e27e625143912e0ce0a891f1312b
unknown <trongtoanbk.hd@gmail.com>
2023-02-24 21:13:29 +0700

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+//    println("Program arguments: ${args.joinToString()}")
+    //1. Khai báo biến
+    //1.1. Muteable: Khai báo biến và biến có thể thay đổi giá trị của biến
+//    // Từ khóa khai báo biến là var
+//    var name = " Phạm tấn phát"
+//    name = "Phạm tấn phát 1"
+//    print(name)
+
+    //1.2. Immutable: Khai báo biến và biến không  thể thay đổi giá trị
+    // từ khóa khai báo val
+//    // constant => Hằng số
+//    val pi =3.14
+//    val address ="199 lý thường kiệt"
+
+}
